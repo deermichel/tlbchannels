ef188c7cb1956b535c7d7eda363552eacd575784
Micha Hanselmann <micha.hanselmann@gmail.com>
2020-04-16 14:14:13 +0200

    working yay

--- src/receiver/receiver.c
+++ src/receiver/receiver.c
@@ -6,6 +6,9 @@
 #include "../packet.h"
 #include "cli.h"
 
+// output buffer size (multiples of payload size)
+#define BUFFER_SIZE 20000
+
 // file descriptor of kernel module proc file
 static int pteaccess_fd = -1;
 
@@ -20,7 +23,7 @@ void open_pteaccess() {
 
 // receive packet
 void receive_packet(packet_t *packet) {
-    // prepare packet
+    // prepare packet - why? descheduled?
     memset(packet->raw, 0xFF, PACKET_SIZE);
 
     // receiver loop
@@ -58,11 +61,15 @@ int main(int argc, char **argv) {
     }
 
     // open output file
-    // FILE *output = fopen(args.filename, "w");
-    // if (output == NULL) {
-    //     printf("error opening output file '%s': %s\n", args.filename, strerror(errno));
-    //     exit(1);
-    // }
+    FILE *output = fopen(args.filename, "w");
+    if (output == NULL) {
+        printf("error opening output file '%s': %s\n", args.filename, strerror(errno));
+        exit(1);
+    }
+
+    // output buffer
+    uint8_t *buffer = malloc(BUFFER_SIZE * PAYLOAD_SIZE);
+    uint32_t offset = 0;
 
     // receiver loop
     packet_t packet;
@@ -84,9 +91,26 @@ int main(int argc, char **argv) {
             for (int i = 0; i < PACKET_SIZE; i++) printf("%02X ", packet.raw[i]);
             printf("\n");
         }
+
+        // save to buffer
+        memcpy(buffer + offset, packet.payload, PAYLOAD_SIZE);
+        offset += PAYLOAD_SIZE;
+
+        // flush buffer
+        if (offset == BUFFER_SIZE * PAYLOAD_SIZE) {
+            fwrite(buffer, 1, offset, output);
+            fflush(output);
+            offset = 0;
+            if (args.verbose) printf("buffer flushed\n");
+        }
     }
 
+    // flush buffer
+    fwrite(buffer, 1, offset, output);
+    fflush(output);
+
     // cleanup
+    fclose(output);
     dealloc_mem(mem, mem_size);
     close(pteaccess_fd);
     return 0;
