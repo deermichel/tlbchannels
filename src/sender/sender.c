40ae3059a9231522a187fcc9c6f142d037cb90c8
Micha Hanselmann <micha.hanselmann@gmail.com>
2020-05-01 11:26:43 +0200

    count time in sender

--- src/sender/sender.c
+++ src/sender/sender.c
@@ -1,5 +1,6 @@
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <time.h>
 #include <unistd.h>
 #include <x86intrin.h>
 #include "../asm.h"
@@ -43,12 +44,12 @@ size_t send_packet(const uint8_t *payload, size_t length) {
     // sender loop
     packet.start = rdtsc();
     for (int i = 0; i < args.window; i++) {
-        for (int set = TLB_SETS - 1; set >= 0; set--) { // from payload to header -> ensure that payload is complete!
+        for (int set = 0; set < TLB_SETS; set++) {
             if (packet.raw[set / 8] & (1 << (set % 8))) {
                 // to send a 1-bit, we evict the whole stlb set by accessing addresses that fall into the respective 
                 // stlb set. we access 16 addresses to ensure full eviction in dtlb + stlb.
-                for (int way = 0; way < 16; way++) {
-                    TOUCH_MEMORY(ADDR(BASE_ADDR, set ^ (TLB_SETS / 2), way));
+                for (int way = 0; way < 17; way++) { // TODO: WHY 17?
+                    TOUCH_MEMORY(ADDR(BASE_ADDR, set ^ (TLB_SETS / 2), way % 16));
                 }
             }
         }
@@ -60,16 +61,19 @@ size_t send_packet(const uint8_t *payload, size_t length) {
 }
 
 // send raw data
-void send_data(const uint8_t *buffer, size_t length) {
+uint32_t send_data(const uint8_t *buffer, size_t length) {
+    uint32_t packets_sent = 0;
     while (length > 0) {
         size_t sent = send_packet(buffer, length);
         buffer += sent;
         length -= sent;
+        packets_sent++;
     }
+    return packets_sent;
 }
 
 // send file
-void send_file(const char* filename) {
+uint32_t send_file(const char* filename) {
     // open file
     int fd = open(filename, O_RDONLY);
     if (fd == -1) {
@@ -95,16 +99,17 @@ void send_file(const char* filename) {
     mlock(buffer, st.st_size);
 
     // send data
-    send_data(buffer, st.st_size);
+    uint32_t packets_sent = send_data(buffer, st.st_size);
 
     // cleanup
     munlock(buffer, st.st_size);
     munmap(buffer, st.st_size);
+    return packets_sent;
 }
 
 // send string
-void send_string(const char *string) {
-    send_data((uint8_t*)string, strlen(string));
+uint32_t send_string(const char *string) {
+    return send_data((uint8_t*)string, strlen(string));
 }
 
 // entry point
@@ -123,21 +128,36 @@ int main(int argc, char **argv) {
         }
     }
 
+    // start time
+    struct timespec start_time;
+    clock_gettime(CLOCK_MONOTONIC, &start_time);
+
     // send data
+    uint32_t packets_sent = 0;
     switch (args.mode) {
         case MODE_SEND_STRING:
-            send_string(args.string);
+            packets_sent = send_string(args.string);
             break;
         case MODE_SEND_FILE:
-            send_file(args.filename);
+            packets_sent = send_file(args.filename);
             break;
     }
+
+    // end time
+    struct timespec end_time;
+    clock_gettime(CLOCK_MONOTONIC, &end_time);
 
     // send data stop
     for (int i = 0; i < 100; i++) send_packet(NULL, 0);
 
     // end logging
     record_packet(NULL);
+
+    // stats
+    double secs = end_time.tv_sec - start_time.tv_sec + (double)(end_time.tv_nsec - start_time.tv_nsec) / 1000000000;
+    printf("packets sent: %d (%d bytes)\n", packets_sent, packets_sent * PAYLOAD_SIZE);
+    printf("bandwidth limit: %.3f kB/s\n", ((packets_sent * PAYLOAD_SIZE) / secs) / 1000.0);
+    printf("time: %f s\n", secs);
 
     // cleanup
     dealloc_mem(mem, mem_size);
