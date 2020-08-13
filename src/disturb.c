72847e8a6b293dde1ff9bc56ab6b114680e952a6
Micha Hanselmann <micha.hanselmann@gmail.com>
2020-08-13 14:12:08 +0000

    yes

--- src/disturb.c
+++ src/disturb.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "asm.h"
-#include "memory.h"
+#include <unistd.h>
 
 // micro-benchmark for tlb interference
 int main(int argc, char **argv) {
     // allocate memory
     size_t num_bytes = 1024L * 1024L * atoi(argv[1]);
+    const int sleep = atoi(argv[2]);
     printf("Allocating %ld MB\n", num_bytes / 1024 / 1024);
-    char *mem = malloc(num_bytes);
-    printf("Running...\n");
+    char *mem = (char*)malloc(num_bytes);
+    printf("Running every %d us...\n", sleep);
 
     // read/writes
     while (1) {
         for (size_t i = 0; i < num_bytes; i += 4096) {
             mem[i]++;
         }
+        if (sleep > 0) usleep(sleep);
     }
 
     // cleanup
